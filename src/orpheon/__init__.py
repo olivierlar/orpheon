0707f8bf0acdaf3d87a11e7bc47fac75ed40ecaf
Olivier Lartillot <olartillot@gmail.com>
2026-04-13 14:29:45 +0200

    Update __init__.py
    quick hack for demo

--- src/orpheon/__init__.py
+++ src/orpheon/__init__.py
@@ -2,4 +2,6 @@ from .api import op
 from .operators import ScalarResult, SpectrumResult
 from .signal import Signal
 
-__all__ = ["op", "Signal", "SpectrumResult", "ScalarResult"]
+audio = op.audio
+
+__all__ = ["op", "Signal", "SpectrumResult", "ScalarResult", "audio"]
